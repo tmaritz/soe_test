0550ea7202915331a14d2cf8af59adeff76e6c9a
tmaritz <github@tmaritz.com>
2018-03-27 22:36:37 +0100

    Initial commit

---
+++ JustEatSOETest/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JustEatSOETest
+{
+    public class FundsManaged
+    {
+        static HttpClient client = new HttpClient();
+
+        //run main method
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        //display results in easily readable console output
+        static void ShowRestaurants(RestaurantsFound.RootObject restaurants)
+        {
+            foreach (var r in restaurants.Restaurants)
+            {
+                Console.WriteLine($"Name: {r.Name}\n\tAverage Rating: {r.RatingAverage}");
+                foreach (var c in r.CuisineTypes)
+                {
+                    Console.WriteLine($"\tType: {c.Name}");
+                }
+            }
+        }
+
+        //process GET request and parse the JSON
+        static async Task<RestaurantsFound.RootObject> GetRestaurantsAsync(string path)
+        {
+            RestaurantsFound.RootObject _restaurants = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<RestaurantsFound.RootObject>(jsonString);
+                ShowRestaurants(data);
+            }
+            return _restaurants;
+        }
+
+        //try running the GET method along with ShowRestaurants using JE's API and headers
+        static async Task RunAsync()
+        {
+            Console.WriteLine("Please enter an outcode:...");
+            var outcode = Console.ReadLine();
+
+            client.BaseAddress = new Uri("https://public.je-apis.com/restaurants?q=" + outcode);
+            client.DefaultRequestHeaders.Add("Accept-Tenant", "uk");
+            client.DefaultRequestHeaders.Add("Accept-Language", "en-GB");
+            client.DefaultRequestHeaders.Add("Authorization", "Basic VGVjaFRlc3RBUEk6dXNlcjI=");
+            client.DefaultRequestHeaders.Add("Host", "public.je-apis.com");
+
+            try
+            {
+                RestaurantsFound.RootObject _restaurants = await GetRestaurantsAsync(client.BaseAddress.ToString());
+                ShowRestaurants(_restaurants);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JustEatSOETest/RestaurantsFound.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustEatSOETest
+{
+    class RestaurantsFound
+    {
+        public class CuisineType
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string SeoName { get; set; }
+        }
+
+        public class Logo
+        {
+            public string StandardResolutionURL { get; set; }
+        }
+
+        public class Restaurant
+        {
+            public List<object> Badges { get; set; }
+            public double? Score { get; set; }
+            public double? DriveDistance { get; set; }
+            public bool? DriveInfoCalculated { get; set; }
+            public string NewnessDate { get; set; }
+            public int? DeliveryMenuId { get; set; }
+            public string DeliveryOpeningTime { get; set; }
+            public double? DeliveryCost { get; set; }
+            public double? MinimumDeliveryValue { get; set; }
+            public object DeliveryTimeMinutes { get; set; }
+            public int? DeliveryWorkingTimeMinutes { get; set; }
+            public DateTime? OpeningTime { get; set; }
+            public string OpeningTimeIso { get; set; }
+            public bool? SendsOnItsWayNotifications { get; set; }
+            public double? RatingAverage { get; set; }
+            public double? Latitude { get; set; }
+            public double? Longitude { get; set; }
+            public List<object> Tags { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Postcode { get; set; }
+            public string City { get; set; }
+            public List<CuisineType> CuisineTypes { get; set; }
+            public string Url { get; set; }
+            public bool? IsOpenNow { get; set; }
+            public bool? IsPremier { get; set; }
+            public bool? IsSponsored { get; set; }
+            public int? SponsoredPosition { get; set; }
+            public bool? IsNew { get; set; }
+            public bool? IsTemporarilyOffline { get; set; }
+            public string ReasonWhyTemporarilyOffline { get; set; }
+            public string UniqueName { get; set; }
+            public bool? IsCloseBy { get; set; }
+            public bool? IsHalal { get; set; }
+            public bool? IsTestRestaurant { get; set; }
+            public int? DefaultDisplayRank { get; set; }
+            public bool? IsOpenNowForDelivery { get; set; }
+            public bool? IsOpenNowForCollection { get; set; }
+            public double? RatingStars { get; set; }
+            public List<Logo> Logo { get; set; }
+            public List<object> Deals { get; set; }
+            public int? NumberOfRatings { get; set; }
+        }
+
+        public class TagDetail
+        {
+            public string Key { get; set; }
+            public string DisplayName { get; set; }
+            public string Colour { get; set; }
+            public string BackgroundColour { get; set; }
+            public int? Priority { get; set; }
+        }
+
+        public class MetaData
+        {
+            public object SearchedTerms { get; set; }
+            public List<TagDetail> TagDetails { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Restaurant> Restaurants { get; set; }
+            public string ShortResultText { get; set; }
+            public string Area { get; set; }
+            public object Errors { get; set; }
+            public bool HasErrors { get; set; }
+            public MetaData MetaData { get; set; }
+        }
+
+    }
+}
